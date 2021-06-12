78421bed9bc34612830d43db4b808eb71d29c0a7
Alex Pelipenko <afpelipenko@omgtu.ru>
2021-06-13 01:58:33 +0600

    Add filtering and error message print

--- src/main/java/servingwebcontent/controllers/CounterpartiesController.java
+++ src/main/java/servingwebcontent/controllers/CounterpartiesController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import servingwebcontent.dto.CounterpartiesDto;
 import servingwebcontent.entity.DictionaryCounterparty;
 import servingwebcontent.repository.DictionaryCounterpartyRepository;
-import servingwebcontent.service.CounterpartiesService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -23,34 +22,51 @@ public class CounterpartiesController {
     private ModelMapper modelMapper = new ModelMapper();
 
     @GetMapping
-    public String getAll(Model model) {
-        model.addAttribute("entities", dictionaryCounterpartyRepo.findAll());
+    public String getAll(@RequestParam(required = false) String name_filter, @RequestParam(required = false) Integer bankBik_filter, @RequestParam(required = false) String accountNumber_filter, Model model) {
+        try {
+            model.addAttribute("name_filter", name_filter);
+            model.addAttribute("bankBik_filter", bankBik_filter);
+            model.addAttribute("accountNumber_filter", accountNumber_filter);
+
+            if (!((bankBik_filter == null && (accountNumber_filter == null || accountNumber_filter.isEmpty())) || (bankBik_filter != null && !accountNumber_filter.isEmpty()))) {
+                model.addAttribute("errorMessage", "БИК и номер счета парные");
+            } else {
+                List<DictionaryCounterparty> filteringList = null;
+                boolean hasNameParam = name_filter != null && !name_filter.isEmpty();
+                if (hasNameParam){
+                    name_filter = "%" + name_filter + "%";
+                }
+
+                boolean hasBankBik = bankBik_filter != null;
+
+                if (hasNameParam && hasBankBik) {
+                    filteringList = dictionaryCounterpartyRepo.findByNameOrAccountNumberAndBankBik(name_filter, accountNumber_filter, bankBik_filter);
+                } else if (hasNameParam) {
+                    filteringList = dictionaryCounterpartyRepo.findByNameLike(name_filter);
+                } else if (hasBankBik) {
+                    filteringList = dictionaryCounterpartyRepo.findByAccountNumberAndBankBik(accountNumber_filter, bankBik_filter);
+                }
+                else{
+                    filteringList = dictionaryCounterpartyRepo.findAll();
+                }
+
+                model.addAttribute("entities", filteringList);
+            }
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+
         return "/main";
     }
 
     @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity save(@RequestBody @Valid CounterpartiesDto counterparties) {
+    public ResponseEntity save(@RequestBody @Valid CounterpartiesDto counterparty) {
         try {
-            DictionaryCounterparty counterpartiesDto = modelMapper.map(counterparties, DictionaryCounterparty.class);
+            DictionaryCounterparty counterpartiesDto = modelMapper.map(counterparty, DictionaryCounterparty.class);
             dictionaryCounterpartyRepo.save(counterpartiesDto);
             return ResponseEntity.ok("Пользователь успешно сохранен");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка " + e.getMessage());
-        }
-    }
-
-    @PostMapping(value = "/filter")
-    public ResponseEntity filter(@RequestBody String text) {
-        try {
-            CounterpartiesService counterpartiesService = new CounterpartiesService();
-            long inn = -1L;
-            if (counterpartiesService.tryParseLong(text)) {
-                inn = Long.parseLong(text);
-            }
-            List<DictionaryCounterparty> sdp = dictionaryCounterpartyRepo.findByNameOrInn(text, inn);
-            return ResponseEntity.ok(sdp);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Не найдено");
         }
     }
 
