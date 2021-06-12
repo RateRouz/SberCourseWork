9e526acfc5ebc56c5ee8f8a383c809bc17f946f0
Alex Pelipenko <afpelipenko@omgtu.ru>
2021-06-12 21:00:13 +0600

    Editing interface

--- src/main/java/servingwebcontent/dto/CounterpartiesDto.java
+++ src/main/java/servingwebcontent/dto/CounterpartiesDto.java
@@ -1,13 +1,19 @@
 package servingwebcontent.dto;
 
 import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
 import servingwebcontent.annotation.CounterpartyValidation;
 
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 @Data
-@CounterpartyValidation(message = "12345678")
+@CounterpartyValidation
 public class CounterpartiesDto {
+    @Id
+    private Long id;
+
+    //@UniqueElements
     @NotBlank(message = "Имя не задано")
     private String name;
 
